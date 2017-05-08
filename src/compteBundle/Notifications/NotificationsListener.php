535f6af804502688ee05acfca8cbafcefce0e88d
eailyass <eailyass@mydailyexperience.com>
2017-05-08 23:26:45 +0100

    listener

--- src/compteBundle/Notifications/NotificationsListener.php
+++ src/compteBundle/Notifications/NotificationsListener.php
@@ -6,6 +6,7 @@ namespace gestionCompteBundle\Notifications;
 
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 
 
 class NotificationsListener
@@ -32,26 +33,24 @@ class NotificationsListener
 
     $this->notificationsHTML = $notificationsHTML;
 
-    $this->notificationList  = 
+    $this->notificationList  = $notificationList;
 
   }
 
 
-  public function processBeta()
+  public function processNotif()
 
   {
 
-    $remainingDays = $this->endDate->diff(new \Datetime())->format('%d');
-
-
-    if ($remainingDays <= 0) {
-
-      // Si la date est dépassée, on ne fait rien
-
+     if (!$event->isMasterRequest()) {
       return;
-
     }
+    $response = $event->getResponse();
 
+ // On utilise notre BetaHRML
+    $response = $this->notificationsHTML->displayNotifications($event->getResponse(), $notificationList);
+    // On met à jour la réponse avec la nouvelle valeur
+    $event->setResponse($response);
     
 
     // Ici on appelera la méthode
